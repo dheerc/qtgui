1d3d25dc9e6263e6d9d7fe49fc0fe28eca16f936
dheerc <dheeruparu@gmail.com>
2014-02-06 14:53:32 -0700

    Initial commit of all qt files

---
+++ qdialog.h
@@ -0,0 +1,22 @@
+#ifndef QDIALOG_H
+#define QDIALOG_H
+
+#include <QDialog>
+#include <QtGui>
+#include <QtCore>
+class QDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit QDialog(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+private:
+
+
+};
+
+#endif // QDIALOG_H
